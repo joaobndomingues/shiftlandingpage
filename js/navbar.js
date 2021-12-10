88db122fdc9cb9cdea37f9f434945b8faab3dd52
unknown <joaobndomingues@gmail.com>
2021-12-10 01:50:53 +0000

    landing page done

---
+++ js/navbar.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll('section');
+const navA = document.querySelectorAll('.navbar a');
+
+window.addEventListener('scroll', () =>{
+    let current = '';
+
+    sections.forEach(section =>{
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if(scrollY > (sectionTop - sectionHeight/3)){
+            current = section.getAttribute('id');
+        }
+    })
+    console.log(current)
+
+    navA.forEach(a =>{
+        a.classList.remove('active');
+        if(a.classList.contains(current)){
+            console.log("FABIO")
+            a.classList.add('active')
+        }
+    })
+})
